f7c053e38389965df4ce812a6e5d2b23adb05d29
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2020-10-30 01:50:38 -0300

    configuração do eslint e css inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const App = () => {
-	return <h1>Gobarber</h1>;
-};
+import GlobalStyle from "./styles/global";
+
+const App = (): React.FC => (
+	<>
+		<h1>Gobarber</h1>
+		<GlobalStyle />
+	</>
+);
 
 export default App;
