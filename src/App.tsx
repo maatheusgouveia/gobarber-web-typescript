fb1c2983c6a0572f9653594b7dc016807474aedf
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2020-11-01 23:45:52 -0300

    configurar eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
-import GlobalStyle from "./styles/global";
+import GlobalStyle from './styles/global';
 
-const App = (): React.FC => (
+const App: React.FC = () => (
 	<>
 		<h1>Gobarber</h1>
 		<GlobalStyle />
---
+++ src/pages/SignIn/index.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => (
+	<Container>
+		<Content></Content>
+		<Background />
+	<Container>
+);
+
+export default SignIn;
