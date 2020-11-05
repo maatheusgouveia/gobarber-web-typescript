b6d67aac659c8c9f91c242fc1dfa748b093b55d9
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2020-11-04 23:52:44 -0300

    corrigir pagina signin

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => {
+	return (
+		<Container>
+			<Content></Content>
+			<Background />
+		</Container>
+	);
+};
+
+export default SignIn;
