668d44a7cc55d58a61fe3271da8dee36149cf33a
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2020-11-07 23:30:45 -0300

    componentizar button e input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,18 @@
+import React, { ComponentType, InputHTMLAttributes } from 'react';
+import { IconBaseProps } from 'react-icons';
+
+import { Container } from './styles';
+
+interface InputPropos extends InputHTMLAttributes<HTMLInputElement> {
+	name: string;
+	icon?: ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputPropos> = ({ icon: Icon, ...rest }) => (
+	<Container>
+		{Icon && <Icon size={20} />}
+		<input {...rest} />
+	</Container>
+);
+
+export default Input;
